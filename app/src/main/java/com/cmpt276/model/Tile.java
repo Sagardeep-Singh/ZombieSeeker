df333efc1fe1b1eaa7917f05d61414bfb379d00a
Sagardeep Singh Hunjan <ssh10@sfu.ca>
2021-10-17 19:26:18 -0700

    Added TileScanObserver and MineRevealObserver interfaces.

--- app/src/main/java/com/cmpt276/model/Tile.java
+++ app/src/main/java/com/cmpt276/model/Tile.java
@@ -1,10 +1,15 @@
 package com.cmpt276.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tile {
 
     private final boolean hasMine;
     private boolean isScanned;
     private boolean isMineRevealed;
+    private List<TileScanObserver> tileScanObservers = new ArrayList<>();
+    private List<MineRevealObserver> mineRevealObservers = new ArrayList<>();
 
     public Tile(boolean hasMine) {
         this.isScanned = false;
@@ -13,22 +18,69 @@ public class Tile {
     }
 
     public boolean hasMine() {
+
         return hasMine;
     }
 
     public boolean isScanned() {
+
         return isScanned;
     }
 
-    public boolean isMineRevealed() {
-        return isMineRevealed;
+    public boolean isMineHidden() {
+
+        return !isMineRevealed;
     }
 
     public void markAsScanned() {
+
         this.isScanned = true;
+        this.notifyTileScanObservers();
     }
 
     public void markAsRevealed() {
+
         isMineRevealed = true;
+        this.notifyMineRevealObservers();
+    }
+
+    public void registerScanObserver(TileScanObserver obs) {
+
+        tileScanObservers.add(obs);
+    }
+
+    public void registerRevealObserver(MineRevealObserver obs) {
+
+        mineRevealObservers.add(obs);
+    }
+
+    public void unregisterScanObserver(TileScanObserver obs) {
+
+        tileScanObservers.remove(obs);
+    }
+
+    public void unregisterRevealObserver(MineRevealObserver obs) {
+
+        mineRevealObservers.remove(obs);
+    }
+
+    private void notifyTileScanObservers() {
+        for (TileScanObserver obs : tileScanObservers) {
+            obs.notifyTileScanned();
+        }
+    }
+
+    private void notifyMineRevealObservers() {
+        for (MineRevealObserver obs : mineRevealObservers) {
+            obs.notifyMineRevealed();
+        }
+    }
+
+    public interface TileScanObserver {
+        void notifyTileScanned();
+    }
+
+    public interface MineRevealObserver {
+        void notifyMineRevealed();
     }
 }
