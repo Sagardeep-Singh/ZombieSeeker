be8b55d17e35386615b2ead8b6ee6097b7368352
Divya Soneji <divya.soneji@hotmail.com>
2021-10-18 19:49:12 -0700

    Handled Saving the selected options

--- app/src/main/java/com/cmpt276/mineseeker/OptionsActivity.java
+++ app/src/main/java/com/cmpt276/mineseeker/OptionsActivity.java
@@ -1,19 +1,21 @@
 package com.cmpt276.mineseeker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.graphics.fonts.Font;
-import android.graphics.fonts.FontFamily;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cmpt276.model.Options;
+
 public class OptionsActivity extends AppCompatActivity {
+
+    private Options options;
 
     public static Intent getIntent(Context context) {
         return new Intent(context, OptionsActivity.class);
@@ -21,10 +23,34 @@ public class OptionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+        options = Options.getInstance();
+
         createBoardDimensionRadioButtons();
         createNumMinesRadioButtons();
+        handleSaveButton();
+    }
+
+    private void handleSaveButton() {
+        Button saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(view -> {
+            updateOptions();
+            finish();
+        });
+    }
+
+
+    private void updateOptions() {
+        RadioGroup dimenGroup = findViewById(R.id.radio_group_board_size);
+        RadioGroup mineGroup = findViewById(R.id.num_mines_radio_group);
+        DimenButton db = findViewById(dimenGroup.getCheckedRadioButtonId());
+        RadioButton rb = findViewById(mineGroup.getCheckedRadioButtonId());
+        options.setNumRows(db.getRow());
+        options.setNumCols(db.getCol());
+        options.setNumMines(Integer.parseInt(rb.getText().toString()));
+        saveOptionsToPreferences();
     }
 
     private void createBoardDimensionRadioButtons() {
@@ -32,20 +58,26 @@ public class OptionsActivity extends AppCompatActivity {
         int []rowOptions = getResources().getIntArray(R.array.board_row_options);
         int []colOptions = getResources().getIntArray(R.array.board_col_options);
         for(int i = 0; i < rowOptions.length; i++){
-           String boardDimens = rowOptions[i] + " by " + colOptions[i];
-            RadioButton dimenButton = new RadioButton(this);
-            setButtonGraphics(dimenButton, boardDimens);
+            DimenButton dimenButton = new DimenButton(this, rowOptions[i], colOptions[i]);
+            setButtonGraphics(dimenButton, dimenButton.getRow() + " by " + dimenButton.getCol());
             group.addView(dimenButton);
+            if((dimenButton.getRow() == this.options.getNumRows())
+                    && (dimenButton.getCol() == this.options.getNumCols())){
+                dimenButton.setChecked(true);
+            }
         }
     }
 
     private void createNumMinesRadioButtons() {
         RadioGroup group = findViewById(R.id.num_mines_radio_group);
         int []numMinesOptions = getResources().getIntArray(R.array.num_mines_options);
-        for(int i = 0; i < numMinesOptions.length; i++){
+        for (int numMinesOption : numMinesOptions) {
             RadioButton numMinesButton = new RadioButton(this);
-            this.setButtonGraphics(numMinesButton, numMinesOptions[i] + "" );
+            this.setButtonGraphics(numMinesButton, numMinesOption + "");
             group.addView(numMinesButton);
+            if (Integer.parseInt(numMinesButton.getText().toString()) == this.options.getNumMines()) {
+                numMinesButton.setChecked(true);
+            }
         }
     }
 
@@ -54,6 +86,35 @@ public class OptionsActivity extends AppCompatActivity {
         rb.setTextColor(Color.WHITE);
         rb.setTextSize(24);
         rb.setButtonTintList(ColorStateList.valueOf(Color.GREEN));
+
+    }
+
+    private void saveOptionsToPreferences() {
+        this.getSharedPreferences(MainActivity.SHARED_PREFERENCES_TAG, MODE_PRIVATE)
+                .edit()
+                .putString(
+                        MainActivity.OPTIONS_TAG,
+                        MainActivity.getGson().toJson(this.options)
+                ).apply();
+    }
+
+    public class DimenButton extends androidx.appcompat.widget.AppCompatRadioButton{
+        private int row;
+        private int col;
+
+        DimenButton(Context context, int row, int col) {
+            super(context);
+            this.row = row;
+            this.col = col;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getCol() {
+            return col;
+        }
 
     }
 }
