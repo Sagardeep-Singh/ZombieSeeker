17aed603c797878403b87c0f0cdf71758d247764
Divya Soneji <divya.soneji@hotmail.com>
2021-10-18 19:21:04 -0700

    Updated Options class

--- app/src/main/java/com/cmpt276/model/Options.java
+++ app/src/main/java/com/cmpt276/model/Options.java
@@ -1,15 +1,39 @@
 package com.cmpt276.model;
 
 public class Options {
-    private int numRows;
-    private int numCols;
-    private int numMines;
+    private final int DEFAULT_ROWS = 4;
+    private final int DEFAULT_COLS = 6;
+    private final int DEFAULT_MINES = 6;
+    private int numRows = DEFAULT_ROWS;
+    private int numCols = DEFAULT_COLS;
+    private int numMines = DEFAULT_MINES;
     private int timesPlayed;
+    private static Options instance;
 
-    Options(int numRows, int numCols, int numMines, int timesPlayed){
+    public static Options getInstance(){
+        if(instance == null){
+             instance = new Options();
+        }
+        return instance;
+    }
+
+    public static void setInstance(Options instance) {
+        Options.instance = instance;
+    }
+
+
+    public Options(){
+    }
+
+    public void setNumRows(int numRows){
         this.numRows = numRows;
+    }
+
+    public void setNumCols(int numCols){
         this.numCols = numCols;
-        this.timesPlayed = timesPlayed;
+    }
+
+    public void setNumMines(int numMines){
         this.numMines = numMines;
     }
 
